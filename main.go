731be28d780e6615bbb45d72d6c92e1ce7e6b8bf
Eliseo Martelli <me@eliseomartelli.it>
2024-09-10 09:39:36 +0200

    chore: add initial scaffolding for custom parsing

--- main.go
+++ main.go
@@ -35,67 +35,89 @@ var (
 
 // Install a dnf package from its packageName.
 func (a *godnf) Install(packageName string, opt *Options) error {
-	return a.runner(func() ([]string, error) {
+	_, err := a.runner(func() ([]string, error) {
 		if strings.TrimSpace(packageName) == "" {
 			return nil, fmt.Errorf("Install: %v", errPackageNameNotSpecified)
 		}
 		return []string{"install", packageName}, nil
+	}, func(string) ([]Package, error) {
+		return nil, nil
 	}, opt)
+	return err
 }
 
 // Update a packages from is packageName. If packageName is empty, updates all
 // the packages in the system.
 func (a *godnf) Update(packageName string, opt *Options) error {
-	return a.runner(func() ([]string, error) {
+	_, err := a.runner(func() ([]string, error) {
 		if strings.TrimSpace(packageName) == "" {
 			return []string{"update"}, nil
 		}
 		return []string{"update", packageName}, nil
+	}, func(string) ([]Package, error) {
+		return nil, nil
 	}, opt)
+	return err
 }
 
 // Obtains a list of dependencies from a packageName.
 func (a *godnf) Depends(packageName string, opt *Options) error {
-	return a.runner(func() ([]string, error) {
+	_, err := a.runner(func() ([]string, error) {
 		if strings.TrimSpace(packageName) == "" {
 			return nil, fmt.Errorf("Depends: %v", errPackageNameNotSpecified)
 		}
 		return []string{"repoquery", "--deplist", packageName}, nil
+	}, func(string) ([]Package, error) {
+		return nil, nil
 	}, opt)
+	return err
 }
 
 // Remove a package from its packageName.
 func (a *godnf) Remove(packageName string, opt *Options) error {
-	return a.runner(func() ([]string, error) {
+	_, err := a.runner(func() ([]string, error) {
 		if strings.TrimSpace(packageName) == "" {
 			return nil, fmt.Errorf("Remove: %v", errPackageNameNotSpecified)
 		}
 		return []string{"remove", packageName}, nil
+	}, func(string) ([]Package, error) {
+		return nil, nil
 	}, opt)
+	return err
 }
 
 // Search a package from its packageName.
 func (a *godnf) Search(packageName string, opt *Options) error {
-	return a.runner(func() ([]string, error) {
+	_, err := a.runner(func() ([]string, error) {
 		if strings.TrimSpace(packageName) == "" {
 			return nil, fmt.Errorf("Remove: %v", errPackageNameNotSpecified)
 		}
 		return []string{}, nil
+	}, func(string) ([]Package, error) {
+		return nil, nil
 	}, opt)
+	return err
 }
 
 // List all installed packages.
 func (a *godnf) List(opt *Options) error {
-	return a.runner(func() ([]string, error) {
+	_, err := a.runner(func() ([]string, error) {
 		return []string{"list", "installed"}, nil
+	}, func(string) ([]Package, error) {
+		return nil, nil
 	}, opt)
+	return err
 }
 
 // runner runs a guest command with opt *Options.
-func (a *godnf) runner(guest func() ([]string, error), opt *Options) error {
+func (a *godnf) runner(
+	guest func() ([]string, error),
+	parser func(string) ([]Package, error),
+	opt *Options,
+) ([]Package, error) {
 	arguments, err := guest()
 	if err != nil {
-		return fmt.Errorf("runner: %v", err)
+		return nil, fmt.Errorf("runner: %v", err)
 	}
 	arguments = append(arguments, processOptions(opt)...)
 	command := execCommander().Command(a.binaryPath, arguments...)
@@ -103,7 +125,7 @@ func (a *godnf) runner(guest func() ([]string, error), opt *Options) error {
 		command.Stdout = opt.Output
 		command.Stderr = opt.Output
 	}
-	return command.Run()
+	return nil, command.Run()
 }
 
 // processOptions returns a slice of command-line flags to be passed to dnf.
@@ -119,4 +141,11 @@ func processOptions(opt *Options) []string {
 		args = append(args, "--assumeyes")
 	}
 	return args
+}
+
+// Package represents a DNF package.
+type Package struct {
+	Name    string
+	Version string
+	Path    string
 }
