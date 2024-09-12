0e9ac83e3298806afa93620c1611f64c5fd227a2
Eliseo Martelli <me@eliseomartelli.it>
2024-09-12 11:01:38 +0200

    chore: refactor internal function calling to use a struct

--- main.go
+++ main.go
@@ -36,104 +36,131 @@ var (
 
 // Install a dnf package from its packageName.
 func (a *godnf) Install(packageName string, opt *Options) error {
-	_, err := a.runner(func() ([]string, error) {
-		if strings.TrimSpace(packageName) == "" {
-			return nil, fmt.Errorf("Install: %v", errPackageNameNotSpecified)
-		}
-		return []string{"install", packageName}, nil
-	}, func(string) ([]Package, error) {
-		return nil, nil
-	}, opt)
+	_, err := a.runner(
+		&runnerParams{
+			argumentBuilder: func() ([]string, error) {
+				if strings.TrimSpace(packageName) == "" {
+					return nil, fmt.Errorf("Install: %v", errPackageNameNotSpecified)
+				}
+				return []string{"install", packageName}, nil
+			},
+			parser: func(string) ([]Package, error) {
+				return nil, nil
+			},
+			opt: opt,
+		})
 	return err
 }
 
 // Update a packages from is packageName. If packageName is empty, updates all
 // the packages in the system.
 func (a *godnf) Update(packageName string, opt *Options) error {
-	_, err := a.runner(func() ([]string, error) {
-		if strings.TrimSpace(packageName) == "" {
-			return []string{"update"}, nil
-		}
-		return []string{"update", packageName}, nil
-	}, func(string) ([]Package, error) {
-		return nil, nil
-	}, opt)
+	_, err := a.runner(&runnerParams{
+		argumentBuilder: func() ([]string, error) {
+			if strings.TrimSpace(packageName) == "" {
+				return []string{"update"}, nil
+			}
+			return []string{"update", packageName}, nil
+		},
+		parser: func(string) ([]Package, error) {
+			return nil, nil
+		},
+		opt: opt,
+	})
 	return err
 }
 
 // Obtains a list of dependencies from a packageName.
 func (a *godnf) Depends(packageName string, opt *Options) error {
-	_, err := a.runner(func() ([]string, error) {
-		if strings.TrimSpace(packageName) == "" {
-			return nil, fmt.Errorf("Depends: %v", errPackageNameNotSpecified)
-		}
-		return []string{"repoquery", "--deplist", packageName}, nil
-	}, func(string) ([]Package, error) {
-		return nil, nil
-	}, opt)
+	_, err := a.runner(&runnerParams{
+		argumentBuilder: func() ([]string, error) {
+			if strings.TrimSpace(packageName) == "" {
+				return nil, fmt.Errorf("Depends: %v", errPackageNameNotSpecified)
+			}
+			return []string{"repoquery", "--deplist", packageName}, nil
+		},
+		parser: func(string) ([]Package, error) {
+			return nil, nil
+		},
+		opt: opt,
+	})
 	return err
 }
 
 // Remove a package from its packageName.
 func (a *godnf) Remove(packageName string, opt *Options) error {
-	_, err := a.runner(func() ([]string, error) {
-		if strings.TrimSpace(packageName) == "" {
-			return nil, fmt.Errorf("Remove: %v", errPackageNameNotSpecified)
-		}
-		return []string{"remove", packageName}, nil
-	}, func(string) ([]Package, error) {
-		return nil, nil
-	}, opt)
+	_, err := a.runner(&runnerParams{
+		argumentBuilder: func() ([]string, error) {
+			if strings.TrimSpace(packageName) == "" {
+				return nil, fmt.Errorf("Remove: %v", errPackageNameNotSpecified)
+			}
+			return []string{"remove", packageName}, nil
+		},
+		parser: func(string) ([]Package, error) {
+			return nil, nil
+		},
+		opt: opt,
+	})
 	return err
 }
 
 // Search a package from its packageName.
 func (a *godnf) Search(packageName string, opt *Options) error {
-	_, err := a.runner(func() ([]string, error) {
-		if strings.TrimSpace(packageName) == "" {
-			return nil, fmt.Errorf("Remove: %v", errPackageNameNotSpecified)
-		}
-		return []string{}, nil
-	}, func(string) ([]Package, error) {
-		return nil, nil
-	}, opt)
+	_, err := a.runner(&runnerParams{
+		argumentBuilder: func() ([]string, error) {
+			if strings.TrimSpace(packageName) == "" {
+				return nil, fmt.Errorf("Remove: %v", errPackageNameNotSpecified)
+			}
+			return []string{}, nil
+		},
+		parser: func(string) ([]Package, error) {
+			return nil, nil
+		},
+		opt: opt,
+	})
 	return err
 }
 
 // List all installed packages.
 func (a *godnf) List(opt *Options) error {
-	_, err := a.runner(func() ([]string, error) {
-		return []string{"list", "installed"}, nil
-	}, func(string) ([]Package, error) {
-		return nil, nil
-	}, opt)
+	_, err := a.runner(&runnerParams{
+		argumentBuilder: func() ([]string, error) {
+			return []string{"list", "installed"}, nil
+		},
+		parser: func(string) ([]Package, error) {
+			return nil, nil
+		},
+		opt: opt,
+	})
 	return err
 }
 
+type runnerParams struct {
+	argumentBuilder func() ([]string, error)
+	parser          func(string) ([]Package, error)
+	opt             *Options
+}
+
 // runner runs a guest command with opt *Options.
-func (a *godnf) runner(
-	guest func() ([]string, error),
-	parser func(string) ([]Package, error),
-	opt *Options,
-) ([]Package, error) {
-	arguments, err := guest()
+func (a *godnf) runner(params *runnerParams) ([]Package, error) {
+	arguments, err := params.argumentBuilder()
 	if err != nil {
 		return nil, fmt.Errorf("runner: %v", err)
 	}
-	arguments = append(arguments, processOptions(opt)...)
+	arguments = append(arguments, processOptions(params.opt)...)
 	command := execCommander().Command(a.binaryPath, arguments...)
 
 	var buffer bytes.Buffer
 	command.Stdout = &buffer
-	if opt.Output != nil {
-		command.Stdout = io.MultiWriter(command.Stdout, opt.Output)
-		command.Stderr = io.MultiWriter(command.Stderr, opt.Output)
+	if params.opt.Output != nil {
+		command.Stdout = io.MultiWriter(command.Stdout, params.opt.Output)
+		command.Stderr = io.MultiWriter(command.Stderr, params.opt.Output)
 	}
 	err = command.Run()
 	if err != nil {
 		return nil, err
 	}
-	parsed, err := parser(buffer.String())
+	parsed, err := params.parser(buffer.String())
 	if err != nil {
 		return nil, err
 	}
